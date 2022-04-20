35c6f8210d8d5c0443e3fe5bed965c6e340eba00
DESKTOP-41K4BMT\DIQUEST <joohyun333@diquest.com>
2022-04-20 19:58:55 +0900

    init

---
+++ problemDao/version1/src/Test.java
@@ -0,0 +1,23 @@
+import java.sql.SQLException;
+
+public class Test {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("아이디1");
+        user.setName("이름");
+        user.setPassword("비밀번호");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + "조회 성공");
+    }
+}
