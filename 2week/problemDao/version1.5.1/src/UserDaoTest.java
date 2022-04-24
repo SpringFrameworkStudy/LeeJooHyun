d89f916a5fab6040ff5139df3d20d5b9a78c70d1
DESKTOP-HVFKF50\joohyun <joohyun@sad.com>
2022-04-25 07:47:14 +0900

    1wnck

---
+++ 2week/problemDao/version1.5.1/src/UserDaoTest.java
@@ -0,0 +1,27 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+
+public class UserDaoTest {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        User user = new User();
+        user.setId("아이디123");
+        user.setName("이름");
+        user.setPassword("비밀번호");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + "조회 성공");
+    }
+}
