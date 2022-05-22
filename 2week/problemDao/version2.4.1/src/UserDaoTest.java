a93ccf1fa0bb28c6c82bc8ebd87221a18bc5cd30
이주현 <joohyun@sad.com>
2022-05-22 13:56:53 +0900

    3.2.2 장까지 공부

--- 2week/problemDao/version2.4.1/src/UserDaoTest.java
+++ 2week/problemDao/version2.4.1/src/UserDaoTest.java
@@ -1,14 +1,24 @@
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.sql.SQLException;
 
 import static org.hamcrest.CoreMatchers.is;
 
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/applicationContext.xml")
 public class UserDaoTest {
+    @Autowired private ApplicationContext context;
+
     private UserDao dao;
     private User user1;
     private User user2;
@@ -16,15 +26,14 @@ public class UserDaoTest {
 
     @Before
     public void setup(){
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         this.dao = context.getBean("userDao", UserDao.class);
-
         this.user1 = new User("아이디133", "이름1", "비밀번호1");
         this.user2 = new User("leegw700", "이름1", "spring2");
         this.user3 = new User("bunjin", "이름이다.", "spring203");
 
-        System.out.println(context);
+        System.out.println(this.context);
         System.out.println(this);
+
     }
 
     @Test
